95b65fd42008c10beb80ff201568c8336162e13f
Moises Coelho <moises.coelho@vnoc.com.br>
2018-05-09 11:14:58 -0300

    Merge entre alguns arquivos JS, alteração na cor do botao veja mais

--- geodarn/js/utilindex.js
+++ geodarn/js/utilindex.js
@@ -13,11 +13,15 @@ function initSliders()
 	},  4000);
 }
 
+/*Método que faz com que o video pare quando a modal é fechada*/
 function stopVideo()
 {
   $("#pinturaMecanizada").attr("src","");
 }
 
+/*Método que da um scroll até o elemento desejado
+ @param element
+*/
 function scrollToElement(elementDiv)
 {
   jQuery("body,html").animate({
@@ -25,7 +29,36 @@ function scrollToElement(elementDiv)
   }, 600);
 }
 
-function initModal()
-{
+/*Aglomerado de funções responsáveis pelos sliders da página*/
+var slideIndex = 1;
+showSlides(slideIndex);
 
+// Next/previous controls
+function plusSlides(n) 
+{
+  showSlides(slideIndex += n);
 }
+
+// Thumbnail image controls
+function currentSlide(n) 
+{
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) 
+{
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1} 
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none"; 
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block"; 
+  dots[slideIndex-1].className += " active";
+}
+
